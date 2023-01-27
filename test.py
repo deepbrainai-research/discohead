a729b4cc14cfbc1b80f103618767007458f71d77
Sunwon Hong <77424795+soonwonh@users.noreply.github.com>
2023-01-27 18:18:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,46 @@
-import torch.nn as nn
+if __name__ == "__main__":
+    import argparse
 
-from modules.utils import AntiAliasInterpolation2d
-from modules.head_predictor import HeadPredictor
-from modules.eyes_encoder import EyesEncoder
-from modules.generator import Generator
+    from inference import inference
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', required=True)
+    args = parser.parse_args()
 
-class Model(nn.Module):
-    def __init__(self, params):
-        super(Model, self).__init__()
-        self.down_sampler = AntiAliasInterpolation2d(3, 0.25) # 1/4 resolution
-        self.head_predictor = HeadPredictor(**params['head_predictor'])
-        self.eyes_encoder = EyesEncoder()
-        self.audio_encoder = params['audio_encoder']()
-        self.generator = Generator(**params['generator'])
+    mode = args.mode
 
+    # parsing mode
+    if mode == 'obama_demo1':
+        from config_obama import params
+        params['mode'] = mode
+        params['dataset_root'] = './dataset/obama/demo1'
+        params['save_path'] = 'obama_demo1.mp4'
+    elif mode == 'obama_demo2':
+        from config_obama import params
+        params['mode'] = mode
+        params['dataset_root'] = './dataset/obama/demo2'
+        params['save_path'] = 'obama_demo2.mp4'
+    elif mode == 'grid_demo1':
+        from config_grid import params
+        params['mode'] = mode
+        params['dataset_root'] = './dataset/grid/demo1'
+        params['save_path'] = 'grid_demo1.mp4'
+    elif mode == 'grid_demo2':
+        from config_grid import params
+        params['mode'] = mode
+        params['dataset_root'] = './dataset/grid/demo2'
+        params['save_path'] = 'grid_demo2.mp4'
+    elif mode == 'koeba_demo1':
+        from config_koeba import params
+        params['mode'] = mode
+        params['dataset_root'] = './dataset/koeba/demo1'
+        params['save_path'] = 'koeba_demo1.mp4'
+    elif mode == 'koeba_demo2':
+        from config_koeba import params
+        params['mode'] = mode
+        params['dataset_root'] = './dataset/koeba/demo2'
+        params['save_path'] = 'koeba_demo2.mp4'
+    else:
+        raise Exception('mode: [obama_demo1|obama_demo2|grid_demo1|grid_demo2|koeba_demo1|koeba_demo2]')
 
-    def forward(self, src, drv, eye, spec):
-        src_down = self.down_sampler(src)
-        drv_down = self.down_sampler(drv)
-
-        src_head = self.head_predictor(src_down)
-        drv_head = self.head_predictor(drv_down)
-
-        drv_eyes = self.eyes_encoder(eye)
-
-        drv_audio = self.audio_encoder(spec)
-
-        generator_out = self.generator(src, src_head, drv_head, drv_eyes, drv_audio)
-
-        return generator_out['prediction']
+    inference(params)
